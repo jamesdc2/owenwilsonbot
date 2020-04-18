a4f2342d0fc012488a339ba7752560caf73f2d5e
James Cline <jrcline09@gmail.com>
2020-04-18 15:02:00 -0400

    Initial commit

---
+++ oh-wow.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`)
+});
+
+client.on('message', message => {
+    // If the message is "ping"
+    if (message.content === 'ping') {
+        // Send "pong" to the same channel
+        message.channel.send('pong');
+    }
+});
+
+client.login(process.env.DISCORD_TOKEN);
