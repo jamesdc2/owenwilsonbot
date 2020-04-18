0bbf7f9385b6134bef92c74cb6bd98a2ed9af767
James Cline <jrcline09@gmail.com>
2020-04-18 15:33:32 -0400

    Sends oh_wow.gif if the message is oh wow

--- oh-wow.js
+++ oh-wow.js
@@ -1,17 +1,21 @@
 require('dotenv').config();
 
-const Discord = require('discord.js');
-const client = new Discord.Client();
+const { Client, MessageAttachment, MessageEmbed} = require("discord.js");
+
+const client = new Client();
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`)
 });
 
 client.on('message', message => {
-    // If the message is "ping"
-    if (message.content === 'ping') {
-        // Send "pong" to the same channel
-        message.channel.send('pong');
+    // If the message is "oh wow"
+    if (message.content.toLowerCase() === 'oh wow') {
+        // Send the gif to the same channel
+        const attachment = new MessageAttachment('https://media.giphy.com/media/ZsQSYaXdrZNm/giphy.gif');
+        message.channel.send(attachment)
+            .then(message => console.log(`Sent message!`))
+            .catch(console.error);
     }
 });
 
