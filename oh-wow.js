4a7cdb226a508e608d0bf324adc237fa7cdc7efd
James Cline <james.cline@xfinion.com>
2021-01-15 14:55:54 -0500

    Added quickie message

--- oh-wow.js
+++ oh-wow.js
@@ -45,6 +45,14 @@ client.on('message', message => {
             .catch(error => console.error(error));
         message.channel.send("@everyone who's thinking about gaming tonight?");
     }
+    if (message.content.toLowerCase() === '!quickie')
+    {
+        const attachment = new MessageAttachment('https://media.giphy.com/media/cCalRsU3yKZoQILEEI/giphy.gif');
+        message.channel.send(attachment)
+            .then(message => console.log(`Sent gif and quickie message to channel #${message.channel.name} at ${moment().format()}!`))
+            .catch(error => console.error(error));
+        message.channel.send("@everyone who's ready to game rn??");
+    }
     if (message.member.roles.cache.find(r => r.name === "cone of shame"))
     {
         message.delete()
