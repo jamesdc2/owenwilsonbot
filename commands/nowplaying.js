3fb9bd63676d5a3e1b9c86d7cefcc44106a80860
James Cline <james.cline@xfinion.com>
2021-04-09 23:45:02 -0400

    Print now playing song and rest of queue

---
+++ commands/nowplaying.js
@@ -0,0 +1,14 @@
+module.exports = {
+	name: 'nowplaying',
+	description: 'Get the song that is playing.',
+	execute(message) {
+		const serverQueue = message.client.queue.get(message.guild.id);
+		if (!serverQueue) return message.channel.send('There is nothing playing.');
+        message.channel.send(`Now playing: ${serverQueue.songs[0].title}`);
+        for ([index, song] of serverQueue.songs.shift())
+        {
+            message.channel.send(`${index}. ${song.title}`)
+        }
+		return message.channel.send(`Now playing: ${serverQueue.songs[0].title}`);
+	},
+};
