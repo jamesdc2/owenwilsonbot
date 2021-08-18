9cf64da924f8a793ccaf4430755bc25310d8772e
James Cline <james.cline@xfinion.com>
2021-08-18 15:17:19 -0400

    Added %change in last 24h

--- commands/crypto.js
+++ commands/crypto.js
@@ -26,8 +26,9 @@ module.exports = {
 			for ( const coin of syms)
 			{
 				var price = data[coin].USD;
+				var dir = price.CHANGEPCTDAY > 0 ? "up" : "down";
 
-				message.channel.send(`$${coin} is currently trading at ${price.PRICE}`)
+				message.channel.send(`$${coin} is currently trading at ${price.PRICE}, *${dir}* ${price.CHANGEPCTDAY}% in the past 24 hours.`)
 				   .then(message => console.log(`Sent ${coin} trading info to #${message.channel.name} at ${moment().format()}!`))
 				   .catch(error => console.error(error));
 			}
