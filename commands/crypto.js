a1f62f5d724e939832e0184e7704d0643feca77f
James Cline <james.cline@xfinion.com>
2021-09-07 14:54:43 -0400

    Displays how much Nicks doge stash is worth

--- commands/crypto.js
+++ commands/crypto.js
@@ -14,7 +14,7 @@ module.exports = {
 	execute(message) {
 
         // code goes here
-		const syms = ["BTC", "DOGE", "ETH"];
+		const syms = ["ETH", "DOGE"];
 		const headers  = { authorization: `Apikey ${CC_API_KEY}` }
 
 		fetch(`${endPoint}/pricemultifull?fsyms=${syms.join(',')}&tsyms=USD`, {
@@ -23,6 +23,7 @@ module.exports = {
 		.then(res => res.json() )
 		.then(data => {
 			data = data.DISPLAY;
+
 			for ( const coin of syms)
 			{
 				var price = data[coin].USD;
@@ -31,7 +32,11 @@ module.exports = {
 				message.channel.send(`$${coin} is currently trading at ${price.PRICE}, **${dir}** ${price.CHANGEPCTDAY}% in the past 24 hours.`)
 				   .then(message => console.log(`Sent ${coin} trading info to #${message.channel.name} at ${moment().format()}!`))
 				   .catch(error => console.error(error));
+
 			}
+
+			var dogePrice = parseFloat(data['DOGE'].USD.PRICE.substring(2));
+			message.channel.send(`@Kneeyuck#6782's dogecoin are currently worth $${406 * dogePrice}`);
 		});
 		
 
